7f83def433eda004d9e7190b3de4b867d91a3868
David Nguyen Blot <d.nguyenblot@gmail.com>
2023-08-21 19:52:00 -0500

    ajout btn trier et filtrer JS

--- Front-End/pieces.js
+++ Front-End/pieces.js
@@ -45,13 +45,51 @@ for (let i = 0; i < pieces.length; i++) {
 	pieceElement.appendChild(stockElement);
 }
 
-//Bouton tri croissant :
+//Bouton tri croissant et decroissant :
 
-const btnTrier = document.querySelector('.btn-trier');
-btnTrier.addEventListener('click', () => {
+const btnTrierPrixCroissants = document.querySelector(
+	'.btn-trier__prix-croissant'
+);
+btnTrierPrixCroissants.addEventListener('click', () => {
 	const piecesOrdonnees = Array.from(pieces); // permet de garder l'ordre d'origine malgrès la modification du tableau.
 	piecesOrdonnees.sort((a, b) => {
 		return a.prix - b.prix;
 	});
 	console.log(piecesOrdonnees);
+});
+
+const btnTrierPrixDecroissants = document.querySelector(
+	'.btn-trier__prix-decroissant'
+);
+
+btnTrierPrixDecroissants.addEventListener('click', () => {
+	const piecesOrdonnees = Array.from(pieces); // permet de garder l'ordre d'origine malgrès la modification du tableau.
+	piecesOrdonnees.sort((a, b) => {
+		return b.prix - a.prix;
+	});
+	console.log(piecesOrdonnees);
+});
+
+// bouton filtre des pieces au prix < ou = à 35 € :
+const btnFiltrerPrixAbordables = document.querySelector(
+	'.btn-filtrer__prix-abordable'
+);
+
+btnFiltrerPrixAbordables.addEventListener('click', () => {
+	const piecesFiltreesPrix = pieces.filter((piece) => {
+		return piece.prix <= 35;
+	});
+	console.log(piecesFiltreesPrix);
+});
+
+// bouton filtre des pieces ayant une description :
+const btnFiltrerNoDescription = document.querySelector(
+	'.btn-filtrer__nodescription'
+);
+
+btnFiltrerNoDescription.addEventListener('click', () => {
+	const piecesFiltreesNoDescription = pieces.filter((piece) => {
+		return piece.description;
+	});
+	console.log(piecesFiltreesNoDescription);
 });
