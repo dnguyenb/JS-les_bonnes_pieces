708e60afe8ff8f44f28e1da82f9b1c3d465f8728
David Nguyen Blot <d.nguyenblot@gmail.com>
2023-08-23 15:49:57 -0500

    ajout pieces abordables et disponibles avec map()

--- Front-End/pieces.js
+++ Front-End/pieces.js
@@ -93,3 +93,43 @@ btnFiltrerNoDescription.addEventListener('click', () => {
 	});
 	console.log(piecesFiltreesNoDescription);
 });
+
+// Liste affichant uniquement le nom des pièces abordables :
+const noms = pieces.map((piece) => piece.nom);
+for (let i = pieces.length - 1; i >= 0; i--) {
+	if (pieces[i].prix > 35) {
+		noms.splice(i, 1);
+	}
+}
+console.log(noms);
+
+// afficher la liste des pièces abordables :
+const abordablesElements = document.createElement('ul');
+
+//Ajout de chaque nom à la liste "ul" :
+for (let i = 0; i < noms.length; i++) {
+	const nomElement = document.createElement('li');
+	nomElement.innerText = noms[i];
+	abordablesElements.appendChild(nomElement);
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables').appendChild(abordablesElements);
+
+// affichage liste des pieces disponibles avec prix :
+const nomsDisponibles = pieces.map((piece) => piece.nom);
+const prixDisponibles = pieces.map((piece) => piece.prix);
+
+for (let i = pieces.length - 1; i >= 0; i--) {
+	if (pieces[i].disponibilite === false) {
+		nomsDisponibles.splice(i, 1);
+		prixDisponibles.splice(i, 1);
+	}
+}
+const disponiblesElement = document.createElement('ul');
+for (let i = 0; i < nomsDisponibles.length; i++) {
+	const nomElement = document.createElement('li');
+	nomElement.innerText = `${nomsDisponibles[i]} - ${prixDisponibles[i]} €`;
+	disponiblesElement.appendChild(nomElement);
+}
+
+document.querySelector('.disponibles').appendChild(disponiblesElement);
